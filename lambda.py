f37420d5503cb822b12006c2a29402831b6a55ef
BRUCE R.YOUNG <64081324+BRUCE-R-YOUNG@users.noreply.github.com>
2023-06-29 17:43:08 +0900

    fisrt

---
+++ lambda.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+import boto3
+from boto3.dynamodb.conditions import Key
+import datetime
+import json
+import traceback
+import os
+
+# -----Dynamo Info change here------
+TABLE_NAME = os.environ.get('TABLE_NAME', "default")
+DDB_PRIMARY_KEY = "DEVICE_NAME"
+DDB_SORT_KEY = "TIMESTAMP"
+# -----Dynamo Info change here------
+
+print(TABLE_NAME)
+
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(TABLE_NAME)
+
+# ------------------------------------------------------------------------
+
+
+def dynamoQuery(DEVICE_NAME, requestTime):
+    print("dynamoQuery start")
+    valList = []
+    res = table.query(
+        KeyConditionExpression=Key(DDB_PRIMARY_KEY).eq(DEVICE_NAME) &
+        Key(DDB_SORT_KEY).lt(requestTime),
+        ScanIndexForward=False,
+        Limit=30
+    )
+
+    for row in res['Items']:
+        val = row['TEMPERATURE']
+        itemDict = {
+            "timestamp": row['TIMESTAMP'],
+            "value": str(val)
+        }
+
+        valList.append(itemDict)
+
+    return valList
+
+# ------------------------------------------------------------------------
+# call by Lambda here.
+#  Event structure : API-Gateway Lambda proxy post
+# ------------------------------------------------------------------------
+
+
+def lambda_handler(event, context):
+    # Lambda Proxy response back template
+    HttpRes = {
+        "statusCode": 200,
+        "headers": {"Access-Control-Allow-Origin": "*"},
+        "body": "",
+        "isBase64Encoded": False
+    }
+
+    try:
+        print("lambda_handler start")
+        print(json.dumps(event))
+
+        # get Parameters
+        # pathParameters = event.get('pathParameters')
+        # print(pathParameters)
+
+        DEVICE_NAME = "temp_humi_press_bruce_20230626"
+        requestTime = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+
+        resItemDict = {DEVICE_NAME: ""}
+        resItemDict[DEVICE_NAME] = dynamoQuery(DEVICE_NAME, requestTime)
+        HttpRes['body'] = json.dumps(resItemDict)
+
+    except Exception as e:
+        print(traceback.format_exc())
+        HttpRes["statusCode"] = 500
+        HttpRes["body"] = "Lambda error. check lambda log"
+
+    print("response:{}".format(json.dumps(HttpRes)))
+    return HttpRes
