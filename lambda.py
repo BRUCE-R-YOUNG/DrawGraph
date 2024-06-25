b6cf55167e4f4fbed4caf4748228d9c538e27a8c
BRUCE R.YOUNG <64081324+BRUCE-R-YOUNG@users.noreply.github.com>
2024-06-25 18:48:39 +0900

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -12,44 +12,44 @@ DDB_PRIMARY_KEY = "DEVICE_NAME"
 DDB_SORT_KEY = "TIMESTAMP"
 # -----Dynamo Info change here------
 
-print(TABLE_NAME)
-
+print("TABLE_NAME:", TABLE_NAME)
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(TABLE_NAME)
 
 # ------------------------------------------------------------------------
 
-
 def dynamoQuery(DEVICE_NAME, requestTime):
     print("dynamoQuery start")
     valList = []
-    res = table.query(
-        KeyConditionExpression=Key(DDB_PRIMARY_KEY).eq(DEVICE_NAME) &
-        Key(DDB_SORT_KEY).lt(requestTime),
-        ScanIndexForward=False,
-        Limit=30
-    )
+    print("Query parameters - DEVICE_NAME: {}, requestTime: {}".format(DEVICE_NAME, requestTime))
+    try:
+        res = table.query(
+            KeyConditionExpression=Key(DDB_PRIMARY_KEY).eq(DEVICE_NAME) &
+            Key(DDB_SORT_KEY).gte(requestTime),  # changed from le to lte
+            ScanIndexForward=False,
+            Limit=30
+        )
+    except Exception as e:
+        print("Error executing query: ", e)
+        raise e
 
-    for row in res['Items']:
-        val = row['TEMPERATURE']
-        itemDict = {
-            "timestamp": row['TIMESTAMP'],
-            "value": str(val)
-        }
+    print("Query result: ", res)
 
-        valList.append(itemDict)
+    if 'Items' in res:
+        for row in res['Items']:
+            if 'device_data' in row and 'TEMPERATURE' in row['device_data']:
+                val = row['device_data']['TEMPERATURE']
+                itemDict = {
+                    "timestamp": row['TIMESTAMP'],
+                    "value": str(val)
+                }
+                valList.append(itemDict)
 
     return valList
 
-# ------------------------------------------------------------------------
-# call by Lambda here.
-#  Event structure : API-Gateway Lambda proxy post
-# ------------------------------------------------------------------------
-
 
 def lambda_handler(event, context):
-    # Lambda Proxy response back template
     HttpRes = {
         "statusCode": 200,
         "headers": {"Access-Control-Allow-Origin": "*"},
@@ -61,12 +61,10 @@ def lambda_handler(event, context):
         print("lambda_handler start")
         print(json.dumps(event))
 
-        # get Parameters
-        # pathParameters = event.get('pathParameters')
-        # print(pathParameters)
-
-        DEVICE_NAME = "temp_humi_press_bruce_20230626"
+        DEVICE_NAME = "temp_humi_press_bruce_20240624"  
         requestTime = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+
+        print("Request Time: ", requestTime)
 
         resItemDict = {DEVICE_NAME: ""}
         resItemDict[DEVICE_NAME] = dynamoQuery(DEVICE_NAME, requestTime)
